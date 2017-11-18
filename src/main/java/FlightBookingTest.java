fb52ce8d388aaa082f5daea7c65964fad65d8cae
mailing2pankaj <30263981+mailing2pankaj@users.noreply.github.com>
2017-11-18 16:39:23 +0530

    Update FlightBookingTest.java

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -12,53 +12,50 @@ import java.util.List;
 
 public class FlightBookingTest {
 
-    WebDriver driver = new ChromeDriver();
-
-
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
         
-        driver.get("https://www.cleartrip.com/");
+        LibraryFunctions.LaunchApp("https://www.cleartrip.com/");
         LibraryFunctions.waitFor(2000);
-        driver.findElement(By.id("OneWay")).click();
+        LibraryFunctions.driver.findElement(By.id("OneWay")).click();
 
-        driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
+        LibraryFunctions.driver.findElement(By.id("FromTag")).clear();
+        LibraryFunctions.driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
 
         //wait for the auto complete options to appear for the origin
 
         LibraryFunctions.waitFor(2000);
-        List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+        List<WebElement> originOptions = LibraryFunctions.driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        LibraryFunctions.driver.findElement(By.id("toTag")).clear();
+        LibraryFunctions.driver.findElement(By.id("toTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
         LibraryFunctions.waitFor(2000);
         //select the first item from the destination auto complete list
-        List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
+        List<WebElement> destinationOptions = LibraryFunctions.driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        LibraryFunctions.driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
 
         //all fields filled in. Now click on search
-        driver.findElement(By.id("SearchBtn")).click();
+        LibraryFunctions.driver.findElement(By.id("SearchBtn")).click();
 
         LibraryFunctions.waitFor(5000);
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
         //close the browser
-        driver.quit();
+        LibraryFunctions.driver.quit();
 
     }
 
 
     private boolean isElementPresent(By by) {
         try {
-            driver.findElement(by);
+            LibraryFunctions.driver.findElement(by);
             return true;
         } catch (NoSuchElementException e) {
             return false;
