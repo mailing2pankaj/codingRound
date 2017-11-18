ef2e1aa6cc81b516cd5f7e7d66c992f43d8c746a
mailing2pankaj <30263981+mailing2pankaj@users.noreply.github.com>
2017-11-18 15:52:24 +0530

    Update FlightBookingTest.java

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -17,10 +17,9 @@ public class FlightBookingTest {
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
-
-        setDriverPath();
+        
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        LibraryFunctions.waitFor(2000);
         driver.findElement(By.id("OneWay")).click();
 
         driver.findElement(By.id("FromTag")).clear();
@@ -28,7 +27,7 @@ public class FlightBookingTest {
 
         //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
+        LibraryFunctions.waitFor(2000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
@@ -37,7 +36,7 @@ public class FlightBookingTest {
 
         //wait for the auto complete options to appear for the destination
 
-        waitFor(2000);
+        LibraryFunctions.waitFor(2000);
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
@@ -47,22 +46,13 @@ public class FlightBookingTest {
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
+        LibraryFunctions.waitFor(5000);
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
         //close the browser
         driver.quit();
 
-    }
-
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
     }
 
 
@@ -75,15 +65,4 @@ public class FlightBookingTest {
         }
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
 }
